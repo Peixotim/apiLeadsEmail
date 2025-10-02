6f21454eb1bcb7635ba126e93c08ce7f345dcc41
Peixotim <pedropeixotovz@gmail.com>
2025-10-02 09:58:50 -0300

    feat:Refatora o ContactModel

--- src/main/java/digital/rj/apileadsandemails/Email/Model/ContactModel.java
+++ src/main/java/digital/rj/apileadsandemails/Email/Model/ContactModel.java
@@ -1,4 +1,13 @@
 package digital.rj.apileadsandemails.Email.Model;
 
-public class ContactModel {
+import digital.rj.apileadsandemails.Leads.Model.Foco;
+
+public record ContactModel (
+        String name,
+        String phone,
+        String email,
+        String enterprise,
+        Foco foco,
+        String body
+){
 }
